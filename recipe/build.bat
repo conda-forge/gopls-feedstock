8da15808a97e8ad4b4a1420cc519b1d1b872ba54
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-26 16:24:19 -0800

    use rattler

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,8 +1,11 @@
+@echo on
+@setlocal EnableDelayedExpansion
+
 cd gopls || goto :error
-go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="-s" || goto :error
+go build -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="-s" || goto :error
 go-licenses save . --save_path=..\license-files --ignore golang.org/x/tools/gopls || goto :error
 
-goto :EOF
+goto :eof
 
 :error
 echo Failed with error #%errorlevel%.
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,5 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 cd gopls
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=../license-files --ignore golang.org/x/tools/gopls
